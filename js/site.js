2cf5fb3011159f85dcfc0e017399840118baa162
RubenMoensJonkersPXL <ruben.moensjonkers@student.pxl.be>
2023-02-21 15:09:31 +0100

    Minor changes in copyright box

--- js/site.js
+++ js/site.js
@@ -1,17 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
+﻿
 AOS.init({
     duration: 1200
 });
-
-// AOS.init({
-//     duration: 500,
-//     easing: 'ease-out-quart',
-//     once: true
-// });
 
 $(".skills").addClass("active")
 $(".skills .skill .skill-bar span").each(function () {
@@ -28,11 +18,13 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip({ trigger: 'manual' }).tooltip('show');
 });
 
-
-
-// $( window ).scroll(function() {   
-// if($( window ).scrollTop() > 10){  // scroll down abit and get the action   
 $(".progress-bar").each(function () {
     each_bar_width = $(this).attr('aria-valuenow');
     $(this).width(each_bar_width + '%');
 });
+
+function getYear() {
+    var today = new Date();
+    var yyyy = today.getFullYear();
+    document.getElementById("copyright").innerHTML = "&copy;" + yyyy + " - Pulsar Project"
+};
